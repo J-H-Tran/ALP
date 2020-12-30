98fca8d2a63d1241b736d56a023411a8151528dd
Jonathan <tr.h.jonathan@gmail.com>
2020-12-29 23:32:14 -0500

    this commit is to show the block scoping of try-catch and why we should initialize the FileWriter class outside the try-catch block when handling IOExceptions when working with files.

--- Projects/FileInputOutput/src/academy/learnprogramming/Locations.java
+++ Projects/FileInputOutput/src/academy/learnprogramming/Locations.java
@@ -11,9 +11,9 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new HashMap<>();
 
     public static void main(String[] args) {
-        FileWriter locFile = null;
+//        FileWriter locFile = null;  // this is declared outside the try-catch because it would have a different scope therefore, would not be useable outside the try-catch blocks
         try {
-            locFile = new FileWriter("locations.txt");
+            FileWriter locFile = new FileWriter("locations.txt");
             for (Location location : locations.values()) {
                 locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
             }
