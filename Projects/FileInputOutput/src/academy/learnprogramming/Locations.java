a9550aa79d733b4b1ecf0705dab7bad3f2288f9b
Jonathan <tr.h.jonathan@gmail.com>
2020-12-30 23:41:46 -0500

    update HashMap to LinkedHashMap for ordering add check to Locations.java to ignore 'Q' as input, scanner bugfix

--- Projects/FileInputOutput/src/academy/learnprogramming/Locations.java
+++ Projects/FileInputOutput/src/academy/learnprogramming/Locations.java
@@ -6,31 +6,35 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Locations implements Map<Integer, Location> {
-    private static Map<Integer, Location> locations = new HashMap<>();
+    private static Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
         // try with resources ensures that FileWriter stream is closed whether the code executes normally or an Exception occurs
-        try (BufferedWriter locFile = new BufferedWriter(new FileWriter("locations2.txt"));
-             BufferedWriter dirFile = new BufferedWriter(new FileWriter("directions2.txt"))) {
+        try (BufferedWriter locFile = new BufferedWriter(new FileWriter("locations.txt"));
+             BufferedWriter dirFile = new BufferedWriter(new FileWriter("directions.txt"))) {
             for (Location location : locations.values()) {
                 locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
 
                 for (String direction : location.getExits().keySet()) {
-                    dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                    if (!direction.equalsIgnoreCase("Q")) {
+                        dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                    }
                 }
             }
         }
-        // try-finally does behave differently than try-with-resources
-        // the try-finally can throw at the .close() back up the call stack
-        // but the above version causes the exception to be suppressed and the exception from the try block is the one thrown up the call stack
-        // possible in the try-finally first error could be hidden by the exception when closing the stream, close() probably not the root cause of error
-        // try-with-resources ensures that the first error is the one being thrown back
+        /*
+        * try-finally does behave differently than try-with-resources
+        * the try-finally can throw at the .close() back up the call stack
+        * but the above version causes the exception to be suppressed and the exception from the try block is the one thrown up the call stack
+        * possible in the try-finally first error could be hidden by the exception when closing the stream, close() probably not the root cause of error
+        * try-with-resources ensures that the first error is the one being thrown back
+        **/
 
     }
 
@@ -50,6 +54,7 @@ public class Locations implements Map<Integer, Location> {
             * scanner must be an object that implements the readable interface.
             * */
         try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations_big.txt")))) {
+            scanner.useDelimiter(",");
             while (scanner.hasNextLine()) {
                 int loc = scanner.nextInt();
 
@@ -58,7 +63,7 @@ public class Locations implements Map<Integer, Location> {
 
                 System.out.println("Imported loc: " + loc + ": " + description);
 
-                Map<String, Integer> tempExit = new HashMap<>();
+                Map<String, Integer> tempExit = new LinkedHashMap<>();
                 locations.put(loc, new Location(loc, description, tempExit));
             }
         } catch (IOException e) {
