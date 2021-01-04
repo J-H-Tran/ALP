13f2718a478ac8ac40f429dff305ac2d276c8836
Jonathan <tr.h.jonathan@gmail.com>
2021-01-03 19:24:54 -0500

    copying a file and copying a directory

--- Projects/JavaCourseTB/src/com/udemy/paths/PathsAndFilesNIOMain.java
+++ Projects/JavaCourseTB/src/com/udemy/paths/PathsAndFilesNIOMain.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class PathsAndFilesNIOMain {
 
@@ -11,10 +12,18 @@ public class PathsAndFilesNIOMain {
         try {
             Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
             Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-            Files.copy(sourceFile, copyFile);
+//            Files.copy(sourceFile, copyFile);   // Errors if file to be copied already exists
+            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING); // 3rd arg will copy file even if it already exists
+
+            // copying a directory, only the folder and not the files it contains. To do that you have to walk the file tree
+            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
+            // moving files
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
