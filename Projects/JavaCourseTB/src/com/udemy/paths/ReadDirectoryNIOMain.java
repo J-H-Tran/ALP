93fe433aad7c36e13c5446a87abc1691c5fc63db
Jonathan <tr.h.jonathan@gmail.com>
2021-01-03 21:27:58 -0500

    DirectoryStream.Filter class to create custom filters for the stream

--- Projects/JavaCourseTB/src/com/udemy/paths/ReadDirectoryNIOMain.java
+++ Projects/JavaCourseTB/src/com/udemy/paths/ReadDirectoryNIOMain.java
@@ -12,11 +12,19 @@ public class ReadDirectoryNIOMain {
 * A directory's content is the list of files that it contains*/
     public static void main(String[] args) {
 
+        DirectoryStream.Filter<Path> filter =
+                new DirectoryStream.Filter<Path>() { // must implement accept() method when using DirectoryStream.Filter
+                                                    // returns true for a path, the path is going to be included in teh DirectoryStream results
+                    public boolean accept(Path path) throws IOException {
+                        return Files.isRegularFile(path); // test to see if the particular path is a regular file
+                    }
+                };
+
         Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
 
         // newDirectoryStream() returns a directory stream of type Path. Returns only directory's direct descendants
         // can filter DirectoryStream for specific file types by specifying 2nd arg using glob syntax for file pattern matching
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
             for (Path file : contents) {
                 System.out.println(file.getFileName());
             }
