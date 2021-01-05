d98872b7465cc4dff6478f8e3edc2575add5447a
Jonathan <tr.h.jonathan@gmail.com>
2021-01-05 07:59:46 -0500

    wrapping up with common problems when working with threads

--- Projects/JavaCourseTB/src/com/udemy/concurrency/LiveLockMain.java
+++ Projects/JavaCourseTB/src/com/udemy/concurrency/LiveLockMain.java
@@ -33,4 +33,26 @@ public class LiveLockMain {
  * if t1 loops as it waits for t2 to complete, and t2 loops waiting for t1 to complete.
  * it could so happen that they both begin looping and are waiting for the other to complete and they never will.
  * t1 and t2 are not blocked though.
+ *
+ * Not really a one size fits all solution. Keep in mind that any time threads have to wait for other threads to
+ * complete something and they don't block while they wait, there is a potential for a live lock.
+ *
+ * Another potential problem when using threads Slip Condition (a specific kind of race condition aka thread interference)
+ * occurs when a thread can be suspended between reading a condition and acting on that condition
+ * If we haven't synchronized the code properly the following can happen:
+ * 1. T1 checks the status and gets OK, suspends
+ * 2. T2 checks the status and gets OK, reads EOF from buffer, sets status to EOF, terminates
+ * 3. T2 runs again, attempts to read from buffer, throws Exception/crashes
+ *
+ * Because the threads can interfere with each other when checking and setting the condition, T1 tried to do something
+ * based on obsolete information, when it checked the stats it was OK.
+ * By the time it acted on the condition it checked previously, the status had been updated by T2. T1 is unaware
+ * and an error occurred.
+ *
+ * similar solution to this as other kinds of thread interference:
+ * - Use synchronized blocks or locks to synchronize the critical sections of code.
+ *
+ * Overview:
+ * - if code is already synchronized then the placement of the synchronization may be the problem
+ * - when using multiple locks, the order in which the locks can be acquired can also result in slipped condition
  * */
