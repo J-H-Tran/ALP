68d36212bc573fedeb33edf0341957b1ca2ed18d
Jonathan <tr.h.jonathan@gmail.com>
2021-01-05 08:05:01 -0500

    static variables are shared across instances

--- Projects/StaticAndInstanceVariables/src/academy/learnprogramming/Main.java
+++ Projects/StaticAndInstanceVariables/src/academy/learnprogramming/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         Dog rex = new Dog("rex");
+        rex.printName();
         Dog fluffy = new Dog("fluffy");
-
         rex.printName();    //prints fluffy
         fluffy.printName(); //prints fluffy
     }
