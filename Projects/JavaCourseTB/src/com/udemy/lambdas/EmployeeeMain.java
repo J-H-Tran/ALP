0a82b4a8b6a90744e786b1c9c782d701f14cdf69
Jonathan <tr.h.jonathan@gmail.com>
2021-01-05 22:37:09 -0500

    update: specific Predicates and test conditions, chaining conditions

--- Projects/JavaCourseTB/src/com/udemy/lambdas/EmployeeeMain.java
+++ Projects/JavaCourseTB/src/com/udemy/lambdas/EmployeeeMain.java
@@ -2,6 +2,9 @@ package com.udemy.lambdas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 
 public class EmployeeeMain {
 
@@ -21,7 +24,7 @@ public class EmployeeeMain {
         employeees.add(red);
         employeees.add(jt);
 
-        System.out.println("\nEmployees over 30:");
+        /*System.out.println("\nEmployees over 30:");
         employeees.forEach(employeee -> {
             if (employeee.getAge() > 30) {
                 System.out.println(employeee.getName());
@@ -32,17 +35,34 @@ public class EmployeeeMain {
             if (employeee.getAge() <= 30) {
                 System.out.println(employeee.getName());
             }
-        });
+        });*/
         /*for (Employeee employee : employeees) {
             if (employee.getAge() > 30) {
                 System.out.println(employee.getName());
             }
         }*/
+        printEmployeesByAge(employeees, "Employee over 30.", employeee -> employeee.getAge() > 30);
+        printEmployeesByAge(employeees, "Employee 30 and under.", employeee -> employeee.getAge() <= 30);
 
-        /*employeees.forEach(employeee -> {
-            System.out.println(employeee.getName());
-            System.out.println(employeee.getAge());
-        });*/
+        IntPredicate greaterThanFifteen = i -> i > 15;
+        IntPredicate lessThanHundred = (i) -> i < 100;
+        System.out.println(greaterThanFifteen.test(10));
+        int a = 15;
+        System.out.println(greaterThanFifteen.test(a + 1));
+
+        System.out.println(greaterThanFifteen.and(lessThanHundred).test(50));
+        System.out.println(greaterThanFifteen.and(lessThanHundred).test(500));
+    }
+
+    private static void printEmployeesByAge(List<Employeee> employeees,
+                                            String ageText,
+                                            Predicate<Employeee> ageCondition) {
+        System.out.println("\n" + ageText);
+        for (Employeee employeee : employeees) {
+            if (ageCondition.test(employeee)) {
+                System.out.println(employeee.getName());
+            }
+        }
     }
 }
 /*
@@ -60,4 +80,19 @@ public class EmployeeeMain {
  * - accept() evaluates lambda body and methods called on obj from arg list
  *
  * ultimately, it's the consumer which was passed as a lambda expression that does the work.
+ * --------------------------------------------------------------------------------------------------------------------
+ * Predicate:
+ * Interface built into java.util.function package
+ * test() - a functional method that accepts a Predicate that returns a boolean
+ * We can replace the if condition we're using in the 2 forEach() with a predicate.
+ *
+ * Predicate args are just lambda expressions that match the Predicate Interface.
+ * they accept 1 arg and return a boolean. This is quite powerful because we can pass all kinds of conditions
+ * to the method and we don't have to write a different if condition to filter the list based on our criteria.
+ * All we need is a new lambda expression that can map to the Predicate Interface.
+ *
+ * In general, Consumer & Predicate Interfaces don't care about the type of args being passed to the lambda expression
+ * They use Generics to infer the type. There are though, Consumers and Predicates that expect a specific type.
+ * double Consumer, int Consumer, long Consumer; similarly with Predicate. Where possible, it's better to use
+ * the more specific type of lambda to improve readability.
  *  */
