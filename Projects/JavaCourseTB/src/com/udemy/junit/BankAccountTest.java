34e09547430d125d28882653da34d01fb843fe96
Jonathan <tr.h.jonathan@gmail.com>
2021-01-06 18:30:21 -0500

    delta, one assertion for every test method

--- Projects/JavaCourseTB/src/com/udemy/junit/BankAccountTest.java
+++ Projects/JavaCourseTB/src/com/udemy/junit/BankAccountTest.java
@@ -1,12 +1,16 @@
 package com.udemy.junit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class BankAccountTest {
 
     @org.junit.Test // this annotation tells Junit that this is a test method
     public void deposit() {
-        fail("This test hasn't been implemented");
+        BankAccount bankAccount = new BankAccount("Jon", "Tran",1000.00);
+        double balance = bankAccount.deposit(200.00,true);
+        // 3rd arg: delta, to specify leeway in comparison. As long as diff between expected and actual is within Delta assertion will Pass
+        assertEquals(1200.00, balance, 0); // when comparing obj, uses equals() to make comparison
     }
 
     @org.junit.Test
@@ -16,7 +20,9 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance() {
-        fail("This test hasn't been implemented");
+        BankAccount bankAccount = new BankAccount("Jon", "Tran",1000.00);
+        double balance = bankAccount.deposit(200.00,true);
+        assertEquals(1200.00, bankAccount.getBalance(), 0);
     }
 
     /*@org.junit.Test
@@ -40,6 +46,14 @@ public class BankAccountTest {
 * - must be annotated with @org.junit.Test
 * - public access modifier
 * - void return type
-*
+* - can the test run and pass on it's own? Answer should always be Yes.
 * Assertion
+*
+* Test methods should be self-contained. Whatever happens in one test method
+* shouldn't depend on what happens in another test method.
+* We can have instance variables in our test class though.
+* Depending on the code it may make sense to create a single instance of a class
+* that we're testing so all the test methods can use when writing tests.
+* Not a best practice for a test method to have multiple assertions.
+* usually 1 per test method.
 * */
