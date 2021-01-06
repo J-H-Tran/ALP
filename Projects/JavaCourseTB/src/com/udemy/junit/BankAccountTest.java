8050ebbc414ad7e58ea9f53fa9f67fd659d75a6a
Jonathan <tr.h.jonathan@gmail.com>
2021-01-06 18:50:48 -0500

    some best practices and assertTrue()

--- Projects/JavaCourseTB/src/com/udemy/junit/BankAccountTest.java
+++ Projects/JavaCourseTB/src/com/udemy/junit/BankAccountTest.java
@@ -1,14 +1,13 @@
 package com.udemy.junit;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
     @org.junit.Test // this annotation tells Junit that this is a test method
     public void deposit() {
-        BankAccount bankAccount = new BankAccount("Jon", "Tran",1000.00);
-        double balance = bankAccount.deposit(200.00,true);
+        BankAccount account = new BankAccount("Jon", "Tran",1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00,true);
         // 3rd arg: delta, to specify leeway in comparison. As long as diff between expected and actual is within Delta assertion will Pass
         assertEquals(1200.00, balance, 0); // when comparing obj, uses equals() to make comparison
     }
@@ -19,10 +18,23 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void getBalance() {
-        BankAccount bankAccount = new BankAccount("Jon", "Tran",1000.00);
-        double balance = bankAccount.deposit(200.00,true);
-        assertEquals(1200.00, bankAccount.getBalance(), 0);
+    public void getBalance_deposit() {
+        BankAccount account = new BankAccount("Jon", "Tran",1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00,true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Jon", "Tran",1000.00, BankAccount.CHECKING);
+        double balance = account.withdraw(200.00,true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Jon", "Tran",1000.00, BankAccount.CHECKING);
+        assertTrue("Account is NOT a CHECKING account",account.isChecking()); // message when fails
     }
 
     /*@org.junit.Test
@@ -56,4 +68,10 @@ public class BankAccountTest {
 * that we're testing so all the test methods can use when writing tests.
 * Not a best practice for a test method to have multiple assertions.
 * usually 1 per test method.
+* A good practice: a test method name should give some indication of the actual
+* condition they are testing.
+*
+* JUnit tests can be run as part of the build process instead of manually.
+* Then the results can be saved to a file where someone can look at the results
+* so messages can be really useful there
 * */
