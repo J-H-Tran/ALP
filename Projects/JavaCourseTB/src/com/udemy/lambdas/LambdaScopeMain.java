7cfc58f9ca677b27fee28ff7a3aab2a73d1fd583
Jonathan <tr.h.jonathan@gmail.com>
2021-01-05 19:43:56 -0500

    update: anon class has no name

---
+++ Projects/JavaCourseTB/src/com/udemy/lambdas/LambdaScopeMain.java
@@ -0,0 +1,72 @@
+package com.udemy.lambdas;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LambdaScopeMain {
+
+    public static void main(String[] args) {
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+
+        AnotherClass anotherClass = new AnotherClass();
+        String s = anotherClass.doSomething();
+        System.out.println(s);
+    }
+
+    public final static String doStringOp(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
+    }
+}
+
+class Employee2 {
+    private String name;
+    private int age;
+
+    public Employee2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+interface UpperConcat2 {
+    public String upperAndConcat(String s1, String s2);
+}
+
+class AnotherClass {
+    public String doSomething() {
+        System.out.println("The AnotherClass's name is: " + getClass().getSimpleName());
+        return Main.doStringOp(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                System.out.println("The anon class's name is: " + getClass().getSimpleName());
+                return s1.toUpperCase() + " " + s2.toUpperCase();
+            }
+        }, "String 1", "String 2");
+    }
+}
